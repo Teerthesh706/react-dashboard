f2bca501b34ce493b8248784d92f0722d4d5d58d
Teerthesh <teerthesh706@gmail.com>
2024-06-09 03:23:49 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('ORg4AjUWIQA/Gnt2UFhhQlJBfVhdVXxLflFyVWBTe1p6cVVWACFaRnZdRl1lSXZScUFnW3tWdnxc'); 
 
 ReactDOM.render(
   <React.StrictMode>
